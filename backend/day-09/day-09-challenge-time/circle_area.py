57aeaaa41ca9a4f6d2df47440a2e9436b314a31a
jkrustia <jkrustia04@gmail.com>
2023-11-29 16:45:48 +0800

    Updated AWSCC-CodeQuest-Backend

---
+++ backend/day-09/day-09-challenge-time/circle_area.py
@@ -0,0 +1,2 @@
+def calculateAreaOfCircle(radius):
+    return 3.14 * (radius * radius)
---
+++ backend/day-09/day-09-challenge-time/main.py
@@ -0,0 +1,6 @@
+from circle_area import calculateAreaOfCircle
+
+radius = int(input("Enter the radius of the circle: "))
+area = calculateAreaOfCircle(radius)
+
+print(f"\nThe area of the circle is {area}")
